38e06fc3ef06f3e3623fe2af9721da287efcdccb
Owen Frere <grinea21@gmail.com>
2019-10-09 12:10:27 +0800

    Initial commit

--- app/src/main/java/com/github/grinea/capitalismisland/MainActivity.java
+++ app/src/main/java/com/github/grinea/capitalismisland/MainActivity.java
@@ -1,10 +1,10 @@
 package com.github.grinea.capitalismisland;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,17 +15,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager fm = getSupportFragmentManager();
+
+        fm.beginTransaction().add(R.id.fragment_holder, new HomeFragment()).commit();
+
         BottomNavigationView botNav = findViewById(R.id.bot_nav_bar);
 
-        botNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener()
-        {
-            @Override
-            public void onNavigationItemReselected(@NonNull MenuItem menuItem)
-            {
-                //change
-            }
-        });
+        botNav.setOnNavigationItemSelectedListener((v) -> {
 
+            Fragment selFrag = null;
+
+                switch (v.getItemId())
+                {
+                    case R.id.nav_home:
+                        selFrag = new HomeFragment();
+                        break;
+                    case R.id.nav_settings:
+                        selFrag = new SettingsFragment();
+                        break;
+                    case R.id.nav_play:
+                        selFrag = new GameFragment();
+                        break;
+                }
+
+                fm.beginTransaction().replace(R.id.fragment_holder, selFrag).commit();
+
+                return true;
+            });
     }
 
 
--- app/src/main/java/com/github/grinea/capitalismisland/model/MapElement.java
+++ app/src/main/java/com/github/grinea/capitalismisland/model/MapElement.java
@@ -7,4 +7,34 @@ public class MapElement
     private Structure structure;
     private Bitmap image;
     private String ownerName;
+
+    public Structure getStructure()
+    {
+        return structure;
+    }
+
+    public void setStructure(Structure structure)
+    {
+        this.structure = structure;
+    }
+
+    public Bitmap getImage()
+    {
+        return image;
+    }
+
+    public void setImage(Bitmap image)
+    {
+        this.image = image;
+    }
+
+    public String getOwnerName()
+    {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName)
+    {
+        this.ownerName = ownerName;
+    }
 }
